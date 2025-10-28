c2742c90235c499ebb5fb8eeb206f80f8b5bcc0d
Alessia S. Fitz Gibbon <asfg@uw.edu>
2025-10-28 09:23:11 -0700

    starter files for forms

---
+++ 12-js-forms/js/starter.js
@@ -0,0 +1,16 @@
+function renderText() {
+  // find the list
+
+  // create an li
+
+  // get the input's text
+  
+  // set li's textContent to input.value
+
+  // add li to the ul
+
+  // reset the input text field
+}
+
+let button = document.querySelector('button');
+button.addEventListener('click', renderText);
